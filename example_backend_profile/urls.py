19e4a072a2b293fad253ce52286083a724cd6836
Aki Koskinen <aki@secondlion.fi>
2020-09-15 13:07:26 +0300

    Provide a basic front page
    The front page always redirects to the admin page.
    
    HP-213

--- example_backend_profile/urls.py
+++ example_backend_profile/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from . import views
 
 urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
     path('', include('social_django.urls', namespace='social')),
     path('helauth/', include('helusers.urls')),
     path('admin/', admin.site.urls),
